7e1754780c2d05f60398f011deb3214a67a9b926
Enrique Folte <37489459+a56z@users.noreply.github.com>
2019-02-27 17:16:45 +0100

    Update react and redux.js

--- react and redux.js
+++ react and redux.js
@@ -42,3 +42,30 @@ class DisplayMessages extends React.Component {
     );
   }
 };
+
+
+// Extract state logic to Redux
+
+const ADD = "ADD";
+
+function addMessage(message){
+  return { 
+   type: ADD,
+   message: message,
+  };
+};
+
+function messageReducer(previousState, action) {
+  return [...previousState, action.message]
+};
+
+
+let store = {
+  state: [],
+  getState: () => store.state, 
+  dispatch: (action) => {
+    if(action.type === ADD){
+      store.state = messageReducer(store.state, action);
+    }
+  }
+};
